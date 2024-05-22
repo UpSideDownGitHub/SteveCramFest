e0243ccef85a1819bb42821cc3a949fcc9394f9d
JarleCorp <mingus.vasseng21@my.northampton.ac.uk>
2024-05-22 10:54:48 +0100

    some hud stuff

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -5,8 +5,13 @@ using UnityEngine.UI;
 public class UI : MonoBehaviour
 {
     public TMP_Text scoreText;
-    public Image[] Hearts;
+    public Animator[] Hearts;
+
+    public Animator gemHolder;
+    private int currentPowerup;
+
     public Image[] powerups;
+    public Image[] powerupsGlow;
     public Color[] powerupColors;
 
     public void SetScore(int score)
@@ -19,9 +24,9 @@ public class UI : MonoBehaviour
         for (int i = 0; i < Hearts.Length; i++)
         {
             if (health > i)
-                Hearts[i].color = Color.red;
+                Hearts[i].SetTrigger("Gain Health");
             else
-                Hearts[i].color = Color.black;
+                Hearts[i].SetTrigger("Lose Health");
         }
     }
 
@@ -30,9 +35,32 @@ public class UI : MonoBehaviour
         for (int i = 0; i < powerups.Length; i++)
         {
             if (types.Length > i)
+            {
+                powerups[i].gameObject.SetActive(true);
+
                 powerups[i].color = powerupColors[(int)types[i]];
+                powerupsGlow[i].color = powerupColors[(int)types[i]];
+            }
             else
+            {
+                powerups[i].gameObject.SetActive(false);
+
                 powerups[i].color = powerupColors[0];
+                powerupsGlow[i].color = powerupColors[0];
+            }
         }
+    }
+
+    public void SetActiveGem()
+    {
+        gemHolder.SetTrigger("Spin");
+
+        powerups[currentPowerup].gameObject.SetActive(false);
+        currentPowerup++;
+
+        if (currentPowerup > 2)
+            currentPowerup = 0;
+
+        powerupsGlow[currentPowerup].gameObject.SetActive(true);
     }
 }
